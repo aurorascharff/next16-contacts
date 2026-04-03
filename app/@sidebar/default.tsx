e9d88f55756957bce262878f1d64135be330b2df
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-03 13:43:06 +0200

    Update

--- app/@sidebar/default.tsx
+++ app/@sidebar/default.tsx
@@ -1,5 +1,8 @@
 import ContactList from '@/components/ContactList';
 
-export default function SidebarDefault() {
-  return <ContactList />;
+export default async function SidebarDefault({ searchParams }: PageProps<'/'>) {
+  const { q } = await searchParams;
+  const query = typeof q === 'string' ? q : undefined;
+
+  return <ContactList q={query} />;
 }
