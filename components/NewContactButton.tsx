d9bb4962e089ae481987eb0fbbac49e7ca8343b6
Aurora Scharff <aurora.sofie@gmail.com>
2026-04-28 22:42:15 +0100

    simplify

--- components/NewContactButton.tsx
+++ components/NewContactButton.tsx
@@ -1,21 +1,13 @@
 'use client';
 
-import React, { startTransition } from 'react';
+import React from 'react';
 import { createEmptyContact } from '@/data/actions/contact';
 
 import SubmitButton from './ui/SubmitButton';
 
 export default function NewContactButton() {
   return (
-    <form
-      action={createEmptyContact}
-      onSubmit={e => {
-        e.preventDefault();
-        startTransition(async () => {
-          await createEmptyContact();
-        });
-      }}
-    >
+    <form action={createEmptyContact}>
       <SubmitButton type="submit" theme="secondary">
         New
       </SubmitButton>
